c72539f310cbe6f112576bab1a0178182791d740
joshboyle <joshboyle@u.boisestate.edu>
2023-09-28 20:50:06 -0600

    nav bar and search

---
+++ Search.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Real Estate Calculator</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Real Estate Calculator</h1>
+        <?php include "nav.php" ?>
+    </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<div id="navigation">
+    <ol>
+        <li><a href="Search.php">Search</a></li>
+        <li><a href="AdvancedSearch.php">Advanced Search</li>
+        <li><a href="about.php">About</li>
+        <li><a href="login.php">LOGIN</li>
+    </ol>
+    <img src="../img/a005-scottw-330.jpg" alt="some house">
+</div>
