89999dbc7fd1abe5b571a58b7b7e861b6706f7c6
joshboyle <joshboyle@u.boisestate.edu>
2023-09-28 21:34:12 -0600

    center image

--- nav.php
+++ nav.php
@@ -1,9 +1,16 @@
 <div id="navigation">
-    <ol>
-        <li><a href="Search.php">Search</a></li>
-        <li><a href="AdvancedSearch.php">Advanced Search</li>
-        <li><a href="about.php">About</li>
-        <li><a href="login.php">LOGIN</li>
-    </ol>
-    <img src="../img/a005-scottw-330.jpg" alt="some house">
+    <div id="nav-left">
+        <ol>
+            <li><a href="Search.php">Search</a></li>
+            <li><a href="AdvancedSearch.php">Advanced Search</li>
+            <li><a href="about.php">About</li>
+        </ol>
+    </div>
+    <div id="nav-right">
+        <li><a href="login-page.php">LOGIN</li>
+    </div>
+</div>
+
+<div id="nav-image">
+    <img src="../img/a005-scottw-330.jpg" alt="some white house in fall">
 </div>
