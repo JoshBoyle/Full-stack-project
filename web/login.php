054e828bba99c39a2c0abbe13a18a7907e81a0cc
joshboyle <joshboyle@u.boisestate.edu>
2023-10-25 20:49:44 -0600

    login checks username and password

--- web/login.php
+++ web/login.php
@@ -1,15 +1,18 @@
 <?php
 // login.php
 
+// TODO need regex to check for valid password
 session_start();
 
 if (isset($_SESSION["access_granted"]) && $_SESSION["access_granted"]) {
     header("Location:granted.php");
 }
-
+// TODO connect this needs the email from the forms find a way to test it
 $email = "";
+$password = "";
 if (isset($_SESSION["email_preset"])) {
     $email = $_SESSION["email_preset"];
+    $password = $_SESSION["password_preset"];
 }
 ?>
 <html lang="en">
@@ -22,7 +25,6 @@ if (isset($_SESSION["email_preset"])) {
   <script defer src="login-page.js"></script>
 </head>
 
-// TODO need regex to check for valid password
 <body>
   <main id="main-holder">
     <h1 id="login-header">Login</h1>
@@ -33,13 +35,13 @@ if (isset($_SESSION["email_preset"])) {
       }
       ?>
     <div id="login-error-msg-holder">
-      <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+      <p id="login-error-msg">Invalid email <span id="error-msg-second-line">and/or password</span></p>
     </div>
 
     <form action="login_handler.php" method="POST">
         <div id="login-form">
-            <input type="text" name="email" id="email" placeholder="Username" value="<?php echo $email; ?>" class="login-form-field" />
-            <input type="password" name="password"  placeholder="Password" id="password" value="" class="login-form-field"/>
+            <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $email; ?>" class="login-form-field" />
+            <input type="password" name="password"  placeholder="Password" id="password" value="<?php echo $password; ?>" class="login-form-field"/>
             <input type="submit" name="submit" id="login-form-submit" value="Login"/>
         </div>
     </form>
--- web/login_handler.php
+++ web/login_handler.php
@@ -2,19 +2,50 @@
 // login_handler.php
 session_start();
 
+
 // TODO for josh@gmail.com and pikachu these need to be variables from sql table
 // For simplification Lets pretend I got these login credentials from an SQL table.
-if ("josh@gmail.com" == $_POST["email"] &&
-    "pikachu" == $_POST["password"]) {
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/";
+
+if ("josh@gmail.com" === $email &&
+    "pikachu" == $password) {
     $_SESSION["access_granted"] = true;
     header("Location:granted.php");
+} elseif (!preg_match($pattern, $email)) {
+    $status = "Not a valid email";
+    $_SESSION["status"] = $status;
+    $_SESSION["email_preset"] = $email;
+    $_SESSION["password_preset"] = $password;
+    $_SESSION["access_granted"] = false;
+    header("Location:login.php");
+
+} elseif ("josh@gmail.com" === $email &&
+        "pikachu" != $password) {
+        $status = "Invalid  password";
+        $_SESSION["status"] = $status;
+        $_SESSION["email_preset"] = $email;
+        $_SESSION["password_preset"] = $password;
+        $_SESSION["access_granted"] = false;
+        header("Location:login.php");
+
+} elseif ("josh@gmail.com" != $email &&
+    "pikachu" == $password) {
+    $status = "Email is not registered";
+    $_SESSION["status"] = $status;
+    $_SESSION["email_preset"] = $email;
+    $_SESSION["password_preset"] = $password;
+    $_SESSION["access_granted"] = false;
+    header("Location:login.php");
 
 } else {
-    $status = "Invalid username or password";
+    $status = "Invalid username and password";
     $_SESSION["status"] = $status;
-    $_SESSION["email_preset"] = $_POST["email"];
+    $_SESSION["email_preset"] = $email;
+    $_SESSION["password_preset"] = $password;
     $_SESSION["access_granted"] = false;
-
     header("Location:login.php");
 }
 ?>
