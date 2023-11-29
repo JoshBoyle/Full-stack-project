aea41e6fdb63083f1feb52cc4b05c1dc576293d2
joshboyle <joshboyle@u.boisestate.edu>
2023-11-29 15:40:01 -0700

    added hashed password ot dao

--- web/UserDao.php
+++ web/UserDao.php
@@ -10,40 +10,33 @@ class UserDao
 
     public function getConnection()
     {
-        return
-            new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
-                $this->pass);
+        return new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
     }
 
-    public function saveUser($email, $user_name, $password, $access, $signUpDate)
+    public function saveUser($email, $user_name, $hashedPassword, $salt, $access, $signUpDate)
     {
         $conn = $this->getConnection();
         $saveQuery =
             "INSERT INTO users
-            (email, 
-             user_name,
-             password, 
-             access, 
-             sign_up_date)
-            VALUES
-            (:email, :user_name, :password, :access, :signUpDate)";
+            (email, user_name, password, salt, access, sign_up_date)
+            VALUES (:email, :user_name, :password, :salt, :access, :signUpDate)";
         $q = $conn->prepare($saveQuery);
         $q->bindParam(':email', $email, PDO::PARAM_STR);
         $q->bindParam(':user_name', $user_name, PDO::PARAM_STR);
-        $q->bindParam(':password', $password, PDO::PARAM_STR);
+        $q->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
+        $q->bindParam(':salt', $salt, PDO::PARAM_STR);
         $q->bindParam(':access', $access, PDO::PARAM_STR);
         $q->bindParam(':signUpDate', $signUpDate, PDO::PARAM_STR);
         $q->execute();
     }
 
-    public function getUserEmail($email)
+    public function getUserByEmail($email)
     {
         try {
             $conn = $this->getConnection();
-            $saveQuery = "SELECT * FROM users WHERE email = :email";
-            $q = $conn->prepare($saveQuery);
+            $selectQuery = "SELECT * FROM users WHERE email = :email";
+            $q = $conn->prepare($selectQuery);
             $q->bindParam(':email', $email, PDO::PARAM_STR);
-
             $q->execute();
             $result = $q->fetch(PDO::FETCH_ASSOC);
             $conn = null;
@@ -52,4 +45,5 @@ class UserDao
             return false;
         }
     }
-} // end Dao
+}
+?>
