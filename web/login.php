cfe11bcaf9dd5f7767040c258a76f24b1e79ee6d
joshboyle <joshboyle@u.boisestate.edu>
2023-11-29 13:00:19 -0700

    fixed login errors

--- web/login.php
+++ web/login.php
@@ -17,7 +17,7 @@ if (isset($_SESSION["email_preset"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="login-page.css">
-    <script defer src="login-page.js"></script>
+<!--    <script defer src="login-page.js"></script>-->
 </head>
 
 <body>
--- web/login_handler.php
+++ web/login_handler.php
@@ -5,6 +5,10 @@ require_once "UserDao.php";
 
 $email = $_POST["email"];
 $password = $_POST["password"];
+echo '<pre>'; print_r($email); echo '</pre>';
+echo '<pre>'; print_r($password); echo '</pre>';
+
+
 $pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/";
 $dao = new UserDao();
 
@@ -17,11 +21,17 @@ try {
     if ($user) {
         if ($user->getEmail() === $email && $user->getPass() === $password) {
             $_SESSION["access_granted"] = true;
-            $_SESSION["email"] = $email;
+            $_SESSION["email_preset"] = $email;
+            $_SESSION["password_preset"] = $password;
             header("location: index.php");
         } elseif (!preg_match($pattern, $email)) {
             handleInvalid("not a valid email");
         } elseif ($user->getEmail() === $email && $user->getPass() !== $password) {
+            echo '<pre>' . 'expected email: '; print_r($email); echo '</pre>';
+            echo '<pre>' . 'actual email: '; print_r($user->getEmail()); echo '</pre>';
+            echo '<pre>' . 'expected pass: '; print_r($password); echo '</pre>';
+            echo '<pre>' . 'actual pass: '; print_r($user->getPass()); echo '</pre>';
+
             handleInvalid("invalid password");
         } else {
             handleInvalid("email is not registered");
@@ -38,6 +48,7 @@ function handleInvalid($status) {
     $_SESSION["email_preset"] = $_POST["email"];
     $_SESSION["password_preset"] = $_POST["password"];
     $_SESSION["access_granted"] = false;
-    header("location: index.php");
+    echo '<pre>'; print_r($status); echo '</pre>';
+//    header("location: index.php");
 }
 ?>
