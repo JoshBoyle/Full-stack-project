baa37ba0501d7e6f12ca5e5c9da2065b5849bbee
joshboyle <joshboyle@u.boisestate.edu>
2023-12-03 22:43:17 -0700

    added debugging function

--- web/login_handler.php
+++ web/login_handler.php
@@ -18,19 +18,26 @@ try {
 
         // Concatenate salt and entered password before hashing
         $hashedEnteredPassword = hash('sha256', $storedSalt . $password);
+//        printPass($hashedEnteredPassword, $storedSalt, $password, );
 
         if ($userData['email'] === $email && $storedPassword === $hashedEnteredPassword) {
             $_SESSION["access_granted"] = true;
             $_SESSION["email_preset"] = $email;
-            header("location: index.php");
+//            header("location: index.php");
+
+            printPass($hashedEnteredPassword, $storedSalt, $password, "access granted");
         } elseif (!preg_match($pattern, $email)) {
+            printPass($hashedEnteredPassword, $storedSalt, $password, "preg_match");
             handleInvalid("Not a valid email");
         } elseif ($userData['email'] === $email && $storedPassword !== $hashedEnteredPassword) {
+            printPass($hashedEnteredPassword, $storedSalt, $password, "invalid pass");
             handleInvalid("Invalid password");
         } else {
+            printPass($hashedEnteredPassword, $storedSalt, $password, "email not regi");
             handleInvalid("Email is not registered");
         }
-    } else {
+    } else { // userData is NUll
+//        printPass($hashedEnteredPassword, $storedSalt, $password, "Invalid email and pass");
         handleInvalid("Invalid email and password");
     }
 } catch (PDOException $e) {
@@ -41,6 +48,13 @@ function handleInvalid($status) {
     $_SESSION["status"] = $status;
     $_SESSION["email_preset"] = $_POST["email"];
     $_SESSION["access_granted"] = false;
-    header("location: index.php");
+//    header("location: index.php");
+}
+
+function printPass($hashedEnteredPassword, $storedSalt, $password, $message) {
+    echo $message;
+    echo "<br>hashed pw:" . $hashedEnteredPassword;
+    echo "<br>stored salt:" . $storedSalt;
+    echo "<br>pw:" . $password;
 }
 ?>
