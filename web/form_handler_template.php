41fe11ff3be5a9f9432d61f76b34c14f9592627b
joshboyle <joshboyle@u.boisestate.edu>
2023-12-04 10:09:29 -0700

    closes #20

--- web/form_handler_template.php
+++ web/form_handler_template.php
@@ -6,20 +6,20 @@ session_start();
 $money_pattern = '/^\$?\d+(?:,\d{3})*(?:\.\d{2})?$/';
 
 // Function to sanitize input
-function sanitize_input($input)
+function sanitize_input($input): string
 {
     return htmlspecialchars(strip_tags(trim($input)), ENT_QUOTES, 'UTF-8');
 }
 
 // Function to convert money format to a numeric value
-function convertMoneyToNumeric($moneyString)
+function convertMoneyToNumeric($moneyString): float
 {
     $moneyString = preg_replace('/[^\d.]/', '', $moneyString);
     return (float)$moneyString;
 }
 
 // Function to validate and sanitize money format
-function validate_money($input, $pattern)
+function validate_money($input, $pattern): false|string
 {
     $sanitized_input = sanitize_input($input);
     return preg_match($pattern, $sanitized_input) ? $sanitized_input : false;
@@ -50,7 +50,12 @@ if (
     $denominator = pow((1 + $r / $n), $n * $t) - 1;
 
     if (isset($_POST["property_tax"])) {
-        // Additional calculations...
+        $_SESSION["property_tax_preset"] = sanitize_input($_POST["property_tax"]);
+        $_SESSION["home_insurance_preset"] = sanitize_input($_POST["home_insurance"]);
+        $_SESSION["hoa_dues_preset"] = sanitize_input($_POST["hoa_dues"]);
+        $prop_tax = (float)$home_price * ((float)$_SESSION["property_tax_preset"]) / 100;
+        $monthly_ins = (float)$_SESSION["home_insurance_preset"];
+        $_SESSION["total"] = ($numerator / $denominator) + ($prop_tax + $monthly_ins + $_SESSION["hoa_dues_preset"]);
     } else {
         $_SESSION["total"] = $numerator / $denominator;
     }
--- web/newadvancedsearch_handler.php
+++ web/newadvancedsearch_handler.php
@@ -1,6 +1,4 @@
 <?php
+//echo "hello";
 require_once "form_handler_template.php";
-if (isset($_SESSION["status"])) {
-    header("Location:AdvancedSearch.php");
-}
-exit();
+header("location:advancedsearch.php");
