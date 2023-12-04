77792e1ea7e44f38737abbebc90a1dad3a261272
JoshBoyle <97471281+JoshBoyle@users.noreply.github.com>
2023-12-04 12:47:47 -0700

    Task24 (#26)
    closes #24

--- web/footer.php
+++ web/footer.php
@@ -7,10 +7,10 @@
             <ul>
                 <li><a href="index.php">Search</a></li>
                 <li><a href="AdvancedSearch.php">Advanced Search</a></li>
+                <a href="logout.php">Logout</a>
 <!--                <li><a href="#">About Us</a></li>-->
 <!--                <li><a href="#">Contact</a></li>-->
                 <p>&copy; 2023 Real Estate Calculator</p>
-                <a href="logout.php">Logout</a>
             </ul>
         </div>
     </div>
