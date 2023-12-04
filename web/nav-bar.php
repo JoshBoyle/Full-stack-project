7b02d2ac4dbf7e9936f64a941135f835c9863dcf
joshboyle <joshboyle@u.boisestate.edu>
2023-12-04 10:47:49 -0700

    nav-bar active logic

--- web/nav-bar.php
+++ web/nav-bar.php
@@ -1,8 +1,14 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
 <nav>
     <ul class="left-nav">
-        <li><a href="index.php">Basic</a></li>
-<!--        <li><a href="NewAdvancedSearch.php">Advanced</a></li>-->
-        <li><a href="AdvancedSearch.php">Advanced</a></li>
+        <li class="nav-button <?php echo ($currentPage === 'index.php') ? 'active' : ''; ?>">
+            <a href="index.php">Basic</a>
+        </li>
+        <li class="nav-button <?php echo ($currentPage === 'AdvancedSearch.php') ? 'active' : ''; ?>">
+            <a href="AdvancedSearch.php">Advanced</a>
+        </li>
     </ul>
     <div class="logo">
         <img src="img/LOGO.png" alt="logo prototype">
