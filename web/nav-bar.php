b13528d268bc7bb0fd282d498fe0fe03fb1a3b02
joshboyle <joshboyle@u.boisestate.edu>
2023-12-04 12:28:31 -0700

    refactored

--- web/nav-bar.php
+++ web/nav-bar.php
@@ -24,22 +24,4 @@ $currentPage = basename($_SERVER['PHP_SELF']);
     </div>
 </nav>
 
-<!--<nav>-->
-<!--    <ul class="left-nav">-->
-<!--        <div id="basic-btn" class="nav-button">-->
-<!--            <li class="active">-->
-<!--                       <li class="nav-button ?php //echo ($currentPage === 'index.php') ? 'active' : ''; ?">-->
-<!--                <a href="index.php">Basic</a>-->
-<!--            </li>-->
-<!--        </div>-->
-<!--        <li class="nav-button --><?php //echo ($currentPage === 'AdvancedSearch.php') ? 'active' : ''; ?><!--">-->
-<!--            <a href="AdvancedSearch.php">Advanced</a>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--    <div class="logo">-->
-<!--        <img src="img/LOGO.png" alt="logo prototype">-->
-<!--    </div>-->
-<!--    <div class="login-button">-->
-<!--        <a href="login.php">Log Out</a>-->
-<!--    </div>-->
-<!--</nav>-->
+
