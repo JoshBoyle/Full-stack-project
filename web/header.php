b443abbbdce91ea1f092842d32bc07e6c599f256
JoshBoyle <97471281+JoshBoyle@users.noreply.github.com>
2023-12-04 12:37:52 -0700

    Task 22 (#25)
    closes #22

--- web/header.php
+++ web/header.php
@@ -1,7 +1,3 @@
 <header>
     <?php include "nav-bar.php" ?>
 </header>
-
-<!--<section id="house-image">-->
-<!--    <img src="img/a005-scottw-330.jpg" alt="White cottage in fall">-->
-<!--</section>-->
--- web/nav-bar.php
+++ web/nav-bar.php
@@ -1,8 +1,20 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
+
 <nav>
     <ul class="left-nav">
-        <li><a href="index.php">Basic</a></li>
-<!--        <li><a href="NewAdvancedSearch.php">Advanced</a></li>-->
-        <li><a href="AdvancedSearch.php">Advanced</a></li>
+        <li>
+            <div id="basic-btn" class="<?php echo ($currentPage === 'index.php') ? 'current-page-button' : 'page-button'; ?>">
+                <a href="index.php">Basic</a>
+            </div>
+        </li>
+        <li>
+
+            <div id="advanced-btn" class="<?php echo ($currentPage === 'AdvancedSearch.php') ? 'current-page-button' : 'page-button'; ?>">
+                <a href="AdvancedSearch.php">Advanced</a>
+            </div>
+        </li>
     </ul>
     <div class="logo">
         <img src="img/LOGO.png" alt="logo prototype">
@@ -11,3 +23,5 @@
         <a href="login.php">Log Out</a>
     </div>
 </nav>
+
+
