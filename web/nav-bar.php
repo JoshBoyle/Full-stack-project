b111d856c89a5cf8ecbd21222617183741f09b0e
joshboyle <joshboyle@u.boisestate.edu>
2023-12-04 12:20:10 -0700

    Basic button color

--- web/nav-bar.php
+++ web/nav-bar.php
@@ -1,13 +1,19 @@
 <?php
 $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
+
 <nav>
     <ul class="left-nav">
-        <li class="nav-button <?php echo ($currentPage === 'index.php') ? 'active' : ''; ?>">
-            <a href="index.php">Basic</a>
+        <li>
+<!--            <div id="basic-btn" class="<php //echo ($currentPage === 'index.php') ? 'active-button' : 'button'; >">-->
+            <div id="basic-btn" class="page-button">
+                <a href="index.php">Basic</a>
+            </div>
         </li>
-        <li class="nav-button <?php echo ($currentPage === 'AdvancedSearch.php') ? 'active' : ''; ?>">
-            <a href="AdvancedSearch.php">Advanced</a>
+        <li>
+            <div id="Advanced" class="<?php echo ($currentPage === 'AdvancedSearch.php') ? 'active-button' : 'button'; ?>">
+                <a href="AdvancedSearch.php">Advanced</a>
+            </div>
         </li>
     </ul>
     <div class="logo">
@@ -17,3 +23,23 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <a href="login.php">Log Out</a>
     </div>
 </nav>
+
+<!--<nav>-->
+<!--    <ul class="left-nav">-->
+<!--        <div id="basic-btn" class="nav-button">-->
+<!--            <li class="active">-->
+<!--                       <li class="nav-button ?php //echo ($currentPage === 'index.php') ? 'active' : ''; ?">-->
+<!--                <a href="index.php">Basic</a>-->
+<!--            </li>-->
+<!--        </div>-->
+<!--        <li class="nav-button --><?php //echo ($currentPage === 'AdvancedSearch.php') ? 'active' : ''; ?><!--">-->
+<!--            <a href="AdvancedSearch.php">Advanced</a>-->
+<!--        </li>-->
+<!--    </ul>-->
+<!--    <div class="logo">-->
+<!--        <img src="img/LOGO.png" alt="logo prototype">-->
+<!--    </div>-->
+<!--    <div class="login-button">-->
+<!--        <a href="login.php">Log Out</a>-->
+<!--    </div>-->
+<!--</nav>-->
